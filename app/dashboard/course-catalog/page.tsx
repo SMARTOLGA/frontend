fb8c6794b41b7b008838060ebc2c1a525e92f933
michael <cmnwankw@andrew.cmu.edu>
2025-02-26 12:11:20 +0200

    added stub links for other sections in the dashbaord

---
+++ app/dashboard/course-catalog/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CourseCatalogPage = () => {
+  return <h3>Course Catalog Page</h3>;
+};
+
+export default CourseCatalogPage;
---
+++ app/dashboard/progress-tracker/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProgressTrackerPage = () => {
+  return <h3>Progress Tracker Page</h3>;
+};
+
+export default ProgressTrackerPage;
